34f814ec87deddfb97597720d74bf139d6c95612
Abdulrahman Adigun <adigunabdulrahman3@gmail.com>
2023-08-11 15:59:28 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,8 +4,8 @@
 	 * Decription - Programming is like building a multilingual puzzle
 	 * Return: Always 0 (success)
 	 */
-int main(void);
-	{
-		puts("\"Programming is like building a multilingual puzzle");
-		return (0);
-	}
+int main(voidi)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
