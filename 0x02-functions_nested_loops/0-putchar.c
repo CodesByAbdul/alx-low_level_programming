6517affe8eede116775d939e35cbb01ec9dccd61
Abdulrahman Adigun <adigunabdulrahman3@gmail.com>
2023-08-15 17:45:41 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Prints putchar followed by new line
+ * Code written by Abdul
+ * Return: 0 Always
+ */
+
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+The content in this directory documents my knowledge of C-Functions, Nested while loop.
