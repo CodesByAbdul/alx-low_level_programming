50066a28c966e4aa328865f1f09ff6b918398be5
Abdulrahman Adigun <adigunabdulrahman3@gmail.com>
2023-08-31 13:40:12 -0700

    0-puts_recursion.c

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,13 @@
+#define "main.h"
+/**
+ * _puts_recursion - this function prints string followed by a new line
+ * Return 0 Always
+ * @s: - string to be printed
+ */
+
+void _puts_recursion(char *s)
+{
+	char s;
+
+	return (s);
+}
