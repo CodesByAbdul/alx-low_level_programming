43bd561c9b3dbbc849ad328219d36d159077f72e
Abdulrahman Adigun <adigunabdulrahman3@gmail.com>
2023-09-01 16:49:45 -0700

    Static-Libraries

---
+++ 0x09-static_libraries/2-strlen.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _strlen - returns the length of a string
+ * @s: stores the string
+ * Coder - CodesByAbdul
+ * Return: 0 Always
+ */
+
+int _strlen(char *s)
+{
+	int length = 0;
+
+	while (*s != '\0')
+	{
+		length++;
+		s++;
+	}
+	return (length);
+}
---
+++ 0x09-static_libraries/3-puts.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _puts - prints strig
+ * @str: hold strigs values
+ * Return: 0 Always
+ */
+
+void _puts(char *str)
+{
+	printf("%s\n", str);
+}
