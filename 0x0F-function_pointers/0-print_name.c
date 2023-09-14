f52d82294fc63440bf1848d34ac01db16a79b809
Abdulrahman Adigun <adigunabdulrahman3@gmail.com>
2023-09-13 19:22:41 -0700

    0-print_name.c

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -3,12 +3,18 @@
 
 /**
  * print_name - prints a name as is
+ * main - main function
+ * custom_print - prints name o
  * @name: name of the person
  * @f: funtion pointer
+ *
  * Return: Nothing.
  */
 
 void print_name(char *name, void (*f)(char *))
 {
+	if (name == NULL || f == NULL)
+		return;
+
 	f(name);
 }
