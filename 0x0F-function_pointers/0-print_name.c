954437c923a88e9e51da7382285b31d2dec09209
Abdulrahman Adigun <adigunabdulrahman3@gmail.com>
2023-09-13 19:33:09 -0700

    0-print_name.c

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,14 +1,11 @@
 #include <stdio.h>
 #include "function_pointers.h"
-
 /**
  * print_name - prints a name as is
  * @name: name of the person
  * @f: funtion pointer
- *
  * Return: Nothing.
  */
-
 void print_name(char *name, void (*f)(char *))
 {
 	if (name == NULL || f == NULL)
