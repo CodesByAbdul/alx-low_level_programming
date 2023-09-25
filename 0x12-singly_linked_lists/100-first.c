10122058a831a60cb56feaf0392123126bbc11e1
CodesByAbdul <adigunabdulrahman3@gmail.com>
2023-09-25 02:51:48 +0100

    Advanced-task

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -9,6 +9,6 @@ void printIntro(void) __attribute__ ((constructor));
 
 void printIntro()
 {
-    printf("You're beat! and yet, you must allow,\n");
-    printf("I bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
 }
