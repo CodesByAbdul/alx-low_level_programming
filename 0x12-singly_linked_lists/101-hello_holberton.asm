1cf0ece779c1d9a3fb9d05d58c11c6f8d9349504
CodesByAbdul <adigunabdulrahman3@gmail.com>
2023-09-25 02:46:57 +0100

    Advanced-task

---
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -0,0 +1,18 @@
+section .data
+    hello_message db "Hello, Holberton",10,0 ; The message to print with a newline character (10)
+
+section .text
+    global main
+
+extern printf
+
+main:
+    push rbp
+    mov rdi, hello_message
+    call printf
+    pop rbp
+
+    ; Exit the program
+    mov rax, 60         ; syscall number for exit
+    xor rdi, rdi        ; exit status 0
+    syscall
